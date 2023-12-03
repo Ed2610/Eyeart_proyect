00cfad8339d070d3f6f8eb02e51d4aafb8d7cebd
Ivanolmos11 <145380126+Ivanolmos11@users.noreply.github.com>
2023-12-03 16:04:36 -0500

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -6,7 +6,7 @@ session_start();
     <head>
         <meta charset="UTF-8">
         <title>E_EArt</title>
-        <link rel ="stylesheet" href="E_ECSS.css">
+        <link rel ="stylesheet" href="CSS/E_ECSS.css">
         <link rel="icon" href="Imagenes/logo1.png" type="image/x-icon">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
