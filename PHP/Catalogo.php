92d170c6e38681202f600113aed6cbbe4c756971
Ivanolmos11 <145380126+Ivanolmos11@users.noreply.github.com>
2023-12-03 16:02:23 -0500

    Update Catalogo.php

--- PHP/Catalogo.php
+++ PHP/Catalogo.php
@@ -4,7 +4,7 @@
 <html>
     <!DOCTYPE = "html">
     <head>
-        <link rel="stylesheet" href="Catalogo.css">
+        <link rel="stylesheet" href="CSS/Catalogo.css">
         <title>E_E Art</title>
         <link rel="icon" href="Imagenes/logo1.png" type="image/x-icon">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
